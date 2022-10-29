1af1515fc1baa87a75935d95b9e387f57ca886e9
milenards <contatomillenarodrigues20@gmail.com>
2022-10-28 21:48:23 -0300

    finalizando o Conversor de moedas

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ function CotacaoDoDia(){
         success: function (dados) {
             let objMoedaExtraida = Object.values(dados)
             let venda = objMoedaExtraida[0].ask
+            venda = new Intl.NumberFormat ('pt-br', {style: 'currency', currency:'BRL'}).format(venda)
             document.getElementById("resultado-cotacao").innerHTML = venda
         }
     });
-
-    
+}
+    function ConverterValor(){
+        let valor = $('#valor-converter').val()
+        $.ajax({
+            type: "GET",
+            url: "https://economia.awesomeapi.com.br/json/last/USD-BRL",
+            dataType: "json",
+            success: function (dados) {
+                let objMoedaExtraida = Object.values(dados)
+                let dolar = objMoedaExtraida[0].ask
+                let convertido = dolar * valor
+                convertido = new Intl.NumberFormat('pt-br',{style:'currency', currency:'BRL'}).format(convertido)
+                $('#valor-final').html(convertido)
+            }
+        });
 }
